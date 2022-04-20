67d0608852cdbf7ad858b40e955656a41e50881a
lapersona <damianmartelletti@gmail.com>
2022-04-20 19:04:07 -0300

    cambios css

--- src/components/BannerSection.jsx
+++ src/components/BannerSection.jsx
@@ -3,12 +3,12 @@ import '../styles/style.blue.css';
 function BannerSection() {
 
     return(
-        <section className='py-5 dark-overlay-xl banner-back'>
+        <section className='py-5 dark-overlay-lg banner-back'>
         <div className='overlay-content'>
-          <div className='container py-4 text-white text-center'>
-            <h2 className='text-uppercase mb-3'>Cirrus Aircraft</h2>
-            <h6 className='lead mb-4'>Somos representantes oficiales de Cirrus Aircraft para Argentina, Bolivia, Paraguay y Uruguay.</h6>
-            <a className='btn btn-outline-light btn-lg' href='http://www.cirrusaircraft.com'>Visite el sitio de Cirrus Aircraft</a>
+          <div className='container py-3 text-white text-center'>
+            <h1 className='text-uppercase mb-3'>Cirrus Aircraft</h1>
+            <h5 className='lead mb-4'>Somos representantes oficiales de Cirrus Aircraft para Argentina, Bolivia, Paraguay y Uruguay.</h5>
+            <a className='btn btn-outline-light' href='http://www.cirrusaircraft.com'>Visite el sitio de Cirrus Aircraft</a>
           </div>
         </div>
       </section>
--- src/components/ServiceSection.jsx
+++ src/components/ServiceSection.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPassport, faPlaneDeparture, faPlane } from '@fortawesome/free-solid-svg-icons';
+import { faQuestion, faPlaneDeparture, faPlane } from '@fortawesome/free-solid-svg-icons';
 import '../styles/style.blue.css';
 import { Link } from 'react-router-dom';
 
@@ -10,18 +10,18 @@ function ServiceSection() {
           <div className='row gy-4'>
             <div className='col-lg-4 col-md-6 block-icon-hover text-center'>
               <div className='icon icon-outlined icon-outlined-primary icon-thin mx-auto mb-3'><Link to='/mantyhabilit'><FontAwesomeIcon icon={faPlane}/></Link></div>
-              <h4 className='text-uppercase mb-3'>MANTENIMIENTO</h4>
-              <p className='text-gray-600 text-sm'>Fifth abundantly made Give sixth hath. Cattle creature i be don't them behold green moved fowl Moved life us beast good yielding. Have bring.</p>
+              <h4 className='text-uppercase mb-4'>MANTENIMIENTO Y HABILITACIONES</h4>
+              <p className='text-gray-800 text-sm'>En Cielo S.A. contamos con todas las habilitaciones otorgadas por ANAC (Administración Nacional de Aviación Civil Argentina) para trabajar con aeronaves, cumpliendo con todas las normas y regulaciones emitidas por la dirección de aeronavegabilidad relativas a talleres aeronáuticos.</p>
             </div>
             <div className='col-lg-4 col-md-6 block-icon-hover text-center'>
-              <div className='icon icon-outlined icon-outlined-primary icon-thin mx-auto mb-3'><Link to='/mantyhabilit'><FontAwesomeIcon icon={faPassport}/></Link></div>
-              <h4 className='text-uppercase mb-3'>HABILITACIONES</h4>
-              <p className='text-gray-600 text-sm'>Advantage old had otherwise sincerity dependent additions. It in adapted natural hastily is justice. Six draw you him full not mean evil. Prepare garrets it expense windows shewing do an.</p>
+              <div className='icon icon-outlined icon-outlined-primary icon-thin mx-auto mb-3'><Link to='/mantyhabilit'><FontAwesomeIcon icon={faQuestion}/></Link></div>
+              <h4 className='text-uppercase mb-4'>QUIENES SOMOS</h4>
+              <p className='text-gray-800 text-sm'>Con mas de 40 años en el mercado aeronautico local, CIELO S.A. es hoy uno de los mayores talleres de mantenimiento de la Argentina.</p>
             </div>
             <div className='col-lg-4 col-md-6 block-icon-hover text-center'>
               <div className='icon icon-outlined icon-outlined-primary icon-thin mx-auto mb-3'><Link to='/ventas'><FontAwesomeIcon icon={faPlaneDeparture}/></Link></div>
-              <h4 className='text-uppercase mb-3'>VENTAS</h4>
-              <p className='text-gray-600 text-sm'>Am terminated it excellence invitation projection as. She graceful shy believed distance use nay. Lively is people so basket ladies window expect.</p>
+              <h4 className='text-uppercase mb-4'>VENTAS</h4>
+              <p className='text-gray-800 text-sm'>Para ventas, complete el formulario y en breve nos contactaremos para asesorarlo o acceda a nuestro e-shop.</p>
             </div>
           </div>
         </div>
