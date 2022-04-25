5afa0194958c572610c4eb57f956a319491c2853
lapersona <damianmartelletti@gmail.com>
2022-04-25 13:54:54 -0300

    cambio responsive img anac

--- src/components/ManteHabilit.jsx
+++ src/components/ManteHabilit.jsx
@@ -34,7 +34,7 @@ function ManteHabilit() {
                         <li><strong>PIPER:</strong> Navajo PA31-300, Navajo PA31-310, Navajo PA31-325, Navajo PA31-350 Chieftain, Navajo PA31P-350 Mojave, Seneca II PA-34-200/T, Seneca III PA34-220T, Cheyenne I PA31-T-500, Cheyenne II PA31-T-620, Cheyenne III PA42-720, CheyennePA42-1000.</li>
                         <li><strong>BRITTEN NORMAN:</strong> BN-2B.</li><br/><br/><br/>
                         <div className='d-flex justify-content-evenly'>
-                            <a href='https://www.anac.gov.ar/anac/web/'><img src={Anac} alt='ANAC Logo'></img></a>
+                            <a href='https://www.anac.gov.ar/anac/web/'><img className='img img-fluid' src={Anac} alt='ANAC Logo'></img></a>
                         </div>
                     </ul>
                 </div>
